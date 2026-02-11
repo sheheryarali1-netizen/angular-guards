663ca4b1a899cc5f9782f8c7e845456a96045a20
sheheryarali1-netizen <sheheryar.ali1@gomotive.com>
2026-02-11 18:15:43 +0500

    Created basic routes for crisis center and heros

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Route[] = [
+  {
+    path: 'crisis-center',
+    component: CrisisListComponent,
+  },
+  {
+    path: 'heroes',
+    component: HeroesListComponent,
+  },
+  { path: '', redirectTo: '/crisis-center', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, CrisisListComponent, HeroesListComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
